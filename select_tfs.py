tf variance bug fix

select_tfs.py
@@ -46,24 +46,24 @@ if __name__ == "__main__":
     variance = np.var(subset, axis = 0)
 
     # make a dataframe and sort the tfs by decreasing variance
-    var_df = pd.DataFrame(columns = ['tf', 'var'])
+    var_df = pd.DataFrame(columns = ['tf', 'variance'])
     var_df.tf = tfs_in_data
-    var_df.var = variance
-    var_df.sort_values(by = 'var', ascending = False, inplace = True)
-    var_df.reset_index(inplace = True)
+    var_df.variance = variance
+    var_df.sort_values(by = 'variance', ascending = False, inplace = True)
+    var_df.reset_index(inplace = True, drop = True)
     
     # use the automatic knee point method to select TFs if there are many
     if len(tfs_in_data) > 40:
 
         # max method
-        kl = kneed.KneeLocator(var_df.index, var_df.var, curve = 'convex', direction = 'decreasing')
+        kl = kneed.KneeLocator(var_df.index, var_df.variance, curve = 'convex', direction = 'decreasing')
         max_y = max(kl.y_difference)
         max_i = np.where(kl.y_difference == max_y)[0][0]
         max_x = kl.x_difference[max_i]
 
         # scale back to original
         x_cutoff = (len(var_df)-1) * max_x
-        y_cutoff = var_df.var[var_df.index == round(x_cutoff)].values[0]
+        y_cutoff = var_df.variance.values[round(x_cutoff)]
 
         # save TF list
         selected_tfs = var_df.tf.to_list()[:x_cutoff+1]
@@ -72,7 +72,7 @@ if __name__ == "__main__":
         # save plot
         fig, ax = plt.subplots(figsize = (6, 4))
         ax.plot(kl.x, kl.y, color = 'k')
-        ax.vlines(x_cutoff, 0, var_df.var.max(), linestyles = "--", colors = 'r')
+        ax.vlines(x_cutoff, 0, var_df.variance.max(), linestyles = "--", colors = 'r')
         ax.hlines(y_cutoff, 0, len(var_df), linestyles = "--", colors = 'r')
         ax.set_ylabel('variance')
         ax.set_xlabel('TF by rank')
@@ -85,7 +85,7 @@ if __name__ == "__main__":
 
         # save plot
         fig, ax = plt.subplots(figsize = (6, 4))
-        ax.plot(var_df.index, var_df.var, color = 'k')
+        ax.plot(var_df.index, var_df.variance, color = 'k')
         ax.set_ylabel('variance')
         ax.set_xlabel('TF by rank')
         fig.savefig(proj_dir + '/ranked_tf_variance.pdf', bbox_inches = 'tight')